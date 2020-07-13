fcafe21fb8b43400844ab5a06b41b216f61b300d
DrBackFire <54931900+DrBackFire@users.noreply.github.com>
2020-07-13 12:21:52 +1000

    added search by any input and created UI class

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const path = require('path');
-const postBooks = require('./Books');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv/config');
+const express = require("express");
+const path = require("path");
+const postBooks = require("./Books");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv/config");
 
 const app = express();
 
@@ -11,28 +11,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 // Setting a static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Body parser
 app.use(bodyParser.json());
 
 //Routes & Middleware
-app.use('/books', postBooks);
+app.use("/books", postBooks);
 
 //Connecting to DB
 mongoose.connect(
   process.env.DB,
   { useUnifiedTopology: true, useNewUrlParser: true },
-  () => console.log('Connected to DB')
+  () => console.log("Connected to DB")
 );
 
 // Setting a Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
-
-// (
-//     'mongodb://localhost/BookList',
-//     { useNewUrlParser: true },
-//     () => console.log('Connected to DB')
-//   );
